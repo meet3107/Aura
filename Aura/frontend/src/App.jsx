fb4a16ed4700ba6b5a7d04edda360d096708d40f
meet3107 <145691752+meet3107@users.noreply.github.com>
2024-03-07 21:46:32 -0500

    5

--- Aura/frontend/src/App.jsx
+++ Aura/frontend/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import './App.css'
+import Bride from './bride'
+import MainPage from './mainpage'
+import Cocktail from "./cocktail";
+import Dolce from "./d&g";
+import Groom from "./groom";
+import Gucci from "./gucci";
+import Lacoste from "./lacoste";
+import Workattire from "./work";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <BrowserRouter>
+    <Routes>
+
+    <Route path="/MainPage" element={<MainPage />} />
+    <Route path="/bride" element={<Bride />} />
+    <Route path="/cocktail" element={<Cocktail />} />
+    <Route path="/d&g" element={<Dolce />} />
+    <Route path="/groom" element={<Groom />} />
+    <Route path="/gucci" element={<Gucci />} />
+    <Route path="/lacoste" element={<Lacoste />} />
+    <Route path="/work" element={<Workattire />} />
+
+
+
+
+
+    </Routes>
+    </BrowserRouter>
+
     </>
   )
 }
